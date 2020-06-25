375927ae7d3207bbd8f3928cbfd0d2e59607409e
Pham The Anh <i.am.teralian@gmail.com>
2020-06-25 12:06:25 +0200

    Live DB connection

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,6 @@
 <?php 
 // After uploading to online server, change this connection accordingly
-$con = mysqli_connect("localhost","root","","ecom");
+$con = mysqli_connect("eu-cdbr-west-03.cleardb.net:3306","b78a198f7d2a64","2c0758f4","heroku_5a06a2225252fd9");
 
 if (mysqli_connect_errno())
   {
